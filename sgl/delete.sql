c8e9ccc71ae9eb4a201a50594c037cc597a869cf
natta1995 <natalie.hallerdal@yahoo.se>
2025-12-12 14:44:47 +0100

    Added updates and delete

---
+++ sgl/delete.sql
@@ -0,0 +1,6 @@
+﻿ 
+ -- Ta bort en användare (exempel: ta bort användare med ID 3)
+
+-- Ta bort en like (exempel: användare 2 ångrar sin like på post 1)
+DELETE FROM Likes
+WHERE UserId = 2 AND PostId = 1;
---
+++ sgl/updates.sql
@@ -0,0 +1,12 @@
+﻿
+-- SQL-skript för att uppdatera data i tabellerna Users och Posts
+
+-- 1. Uppdatera ett användarnamn
+UPDATE Users
+SET Username = 'anna_updated'
+WHERE Id = 1;
+
+-- 2. Uppdatera texten i ett inlägg
+UPDATE Posts
+SET PostText = 'Hello, this is my updated first post!'
+WHERE Id = 1;
